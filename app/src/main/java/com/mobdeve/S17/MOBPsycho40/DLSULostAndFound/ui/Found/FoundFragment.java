fd1a07e4c2e2d58dea0b729599638e1a1d5b164a
Tean Concio <tean_concio@dlsu.edu.ph>
2024-10-08 03:25:23 +0800

    Renamed existing tabs and added profile tab

--- app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Found/FoundFragment.java
+++ app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Found/FoundFragment.java
@@ -1,4 +1,4 @@
-package com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ui.home;
+package com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ui.Found;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.databinding.FragmentHomeBinding;
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.databinding.FragmentFoundBinding;
 
-public class HomeFragment extends Fragment {
+public class FoundFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentFoundBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        FoundViewModel foundViewModel =
+                new ViewModelProvider(this).get(FoundViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentFoundBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textFound;
+        foundViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Lost/LostViewModel.java
+++ app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Lost/LostViewModel.java
@@ -1,16 +1,16 @@
-package com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ui.notifications;
+package com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ui.Lost;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class LostViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public LostViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is lost fragment");
     }
 
     public LiveData<String> getText() {
