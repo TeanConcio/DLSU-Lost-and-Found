f95b043f2f0e086aaaa118db9c0d61cc47d4abf2
Xiao-Alatus <113570899+Xiao-Alatus@users.noreply.github.com>
2024-10-08 23:28:14 +0800

    Changed the Found Item Activity Views

--- app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Found/FoundFragment.java
+++ app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Found/FoundFragment.java
@@ -1,15 +1,18 @@
 package com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ui.Found;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.FoundItemActivity;
 import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.databinding.FragmentFoundBinding;
 
 public class FoundFragment extends Fragment {
@@ -26,7 +29,25 @@ public class FoundFragment extends Fragment {
 
         final TextView textView = binding.textFound;
         foundViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
+        //TODO: FOR TESTING ONLY, YOU CAN REMOVE IT
+        final Button actionButton = binding.buttonAction;  // Bind the button from XML
+        actionButton.setOnClickListener(v-> {
+            Intent intent = new Intent(getActivity(), FoundItemActivity.class);
+            startActivity(intent);
+
+        });
+
+
+
+
+
+
+
         return root;
+
     }
 
     @Override
