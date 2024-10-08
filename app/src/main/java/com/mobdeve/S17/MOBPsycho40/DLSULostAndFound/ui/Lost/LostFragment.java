d235dbef0406c8ddf45fed7ef60b226aad220779
Tean Concio <tean_concio@dlsu.edu.ph>
2024-10-09 03:00:58 +0800

    Created basic Lost Tab Layout

--- app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Lost/LostFragment.java
+++ app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Lost/LostFragment.java
@@ -2,14 +2,18 @@ package com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ui.Lost;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.R;
 import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.databinding.FragmentLostBinding;
 
 public class LostFragment extends Fragment {
@@ -18,14 +22,18 @@ public class LostFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        LostViewModel lostViewModel =
-                new ViewModelProvider(this).get(LostViewModel.class);
 
         binding = FragmentLostBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textLost;
-        lostViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        LostViewModel lostViewModel =
+//                new ViewModelProvider(this).get(LostViewModel.class);
+
+//        final TextView textView = binding.textLost;
+//        lostViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        binding.lostFilter1.setOnTouchListener(this::onTouch);
+
         return root;
     }
 
@@ -33,5 +41,13 @@ public class LostFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.filter_press);
+            v.startAnimation(anim);
+        }
+        return false;
     }
 }
