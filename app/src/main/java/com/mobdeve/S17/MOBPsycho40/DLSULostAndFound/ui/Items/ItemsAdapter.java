fead76cff0feb552bd2b9877b5872f307629f5bb
Xiao-Alatus <113570899+Xiao-Alatus@users.noreply.github.com>
2024-10-18 01:49:58 +0800

    add: recycler view for my requests

---
+++ app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Items/ItemsAdapter.java
@@ -0,0 +1,91 @@
+package com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ui.Items;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ItemActivity;
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.R;
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.models.LostItem;
+
+import java.time.format.DateTimeFormatter;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+    LostItem[] myLostItemList;
+    Context context;
+
+    public ItemsAdapter(LostItem[] lostItemList, FragmentActivity activity) {
+        this.myLostItemList = lostItemList;
+        this.context = activity;
+    }
+
+    @NonNull
+    @Override
+    public ItemsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.list_item_lost_item,parent,false);
+        ItemsAdapter.ViewHolder viewHolder = new ItemsAdapter.ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemsAdapter.ViewHolder holder, int position) {
+        final LostItem currentItem = myLostItemList[position];
+        holder.lostItemImage.setImageResource(currentItem.getImage());
+        holder.lostItemName.setText(currentItem.getName());
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        holder.lostItemDate.setText(currentItem.getDateLost().format(dateTimeFormatter));
+        holder.lostItemLocation.setText(currentItem.getLocation());
+        holder.lostItemDescription.setText(currentItem.getDescription());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(context, "Display " + currentItem.getName() + " information", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(context, ItemActivity.class);
+                i.putExtra("image",currentItem.getImage());
+                i.putExtra("name",currentItem.getName());
+                i.putExtra("category",currentItem.getCategory());
+                i.putExtra("date",currentItem.getDateLost().format(dateTimeFormatter));
+                i.putExtra("campus", currentItem.getCampus());
+                i.putExtra("location",currentItem.getLocation());
+                i.putExtra("description", currentItem.getDescription());
+                context.startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return myLostItemList.length;
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView lostItemImage;
+        TextView lostItemName;
+        TextView lostItemDate;
+        TextView lostItemLocation;
+        TextView lostItemDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            lostItemImage = itemView.findViewById(R.id.lostItemImage);
+            lostItemName = itemView.findViewById(R.id.lostItemName);
+            lostItemDate = itemView.findViewById(R.id.lostItemDate);
+            lostItemLocation = itemView.findViewById(R.id.lostItemLocation);
+            lostItemDescription = itemView.findViewById(R.id.lostItemDescription);
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Items/ItemsFragment.java
+++ app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/ui/Items/ItemsFragment.java
@@ -9,8 +9,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.R;
 import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.databinding.FragmentItemsBinding;
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.models.LostItem;
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.ui.Lost.LostItemAdapter;
+
+import java.time.LocalDateTime;
 
 public class ItemsFragment extends Fragment {
 
@@ -18,14 +24,33 @@ public class ItemsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ItemsViewModel itemsViewModel =
-                new ViewModelProvider(this).get(ItemsViewModel.class);
 
         binding = FragmentItemsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textItems;
-        itemsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        ItemsViewModel itemsViewModel =
+//                new ViewModelProvider(this).get(ItemsViewModel.class);
+//
+//        final TextView textView = binding.textItems;
+//        itemsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        // RecyclerView
+        binding.myLostItemRecycler.setHasFixedSize(true);
+        binding.myLostItemRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        LostItem[] lostItemList = new LostItem[]{
+                new LostItem("Dominic \"THE GOAT\" Sia says that: According to all known laws of aviation, a bee isn't supposed to be able to fly", "Electronics", "It is the phone owned by the one and only Gojo \"Dominic Sia\" Satoru. It has a blue, red, and purple design and contains LIMITLESS (Cursed) Energy", "Manila", "Br. Andrew Hall wants to know your location right here right now", R.drawable.sample_the_goat, LocalDateTime.now()),
+                new LostItem("iPhone 69 LIMITLESS", "Electronics", "It is the phone owned by the one and only Gojo \"Dominic Sia\" Satoru. It has a blue, red, and purple design and contains LIMITLESS (Cursed) Energy", "Manila", "Henry Sy", R.drawable.sample_iphone16_pro_max, LocalDateTime.now()),
+                new LostItem("Airpods Pro", "Electronics", "It is the airpods owned by the one and only Gojo \"Dominic Sia\" Satoru. It has a blue, red, and purple design and contains LIMITLESS (Cursed) Energy", "Manila", "Henry Sy", R.drawable.sample_airpods_pro, LocalDateTime.now()),
+        };
+
+        // RecyclerView Adapter
+        ItemsAdapter itemsAdapter = new ItemsAdapter(lostItemList, getActivity());
+        binding.myLostItemRecycler.setAdapter(itemsAdapter);
+
+
+
         return root;
     }
 
