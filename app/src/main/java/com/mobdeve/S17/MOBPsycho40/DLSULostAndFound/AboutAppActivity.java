ca333cc5e34238b8f153c71a248e45e399cf8b68
dominicsia <siadominic@gmail.com>
2024-10-18 12:25:10 +0800

    add: About app activity

--- app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/AboutAppActivity.java
+++ app/src/main/java/com/mobdeve/S17/MOBPsycho40/DLSULostAndFound/AboutAppActivity.java
@@ -8,17 +8,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.mobdeve.S17.MOBPsycho40.DLSULostAndFound.databinding.ActivityAboutAppBinding;
+
 public class AboutAppActivity extends AppCompatActivity {
+
+    private ActivityAboutAppBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        binding = ActivityAboutAppBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         setContentView(R.layout.activity_about_app);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.btnBack.setOnClickListener(v -> {
+            finish();
         });
     }
 }
